30bcca70127e97dbc44318cecd902fb6c1ddb135
Rob Higgins <higginsrob@gmail.com>
2017-10-31 17:10:36 -0700

    add doccco documentation

---
+++ README.md
@@ -0,0 +1,308 @@
+
+
+
+
+# JDOM.js
+
+_lightweight dom builder_
+
+---
+
+
+
+
+
+
+
+##### installation
+
+```
+
+npm install jdom
+
+```
+
+#### usage
+
+_intended usage with webpack or browserify_
+
+```
+const {
+   DIV, SPAN, SCRIPT, STYLE, // ...etc
+   createElement, updateElement,
+   id, $, $$,
+   on, once, off, dispatch,
+   currentScript,
+   style,
+   isObject, isArray, isElement
+} from 'jdom';
+```
+
+---
+
+
+
+
+
+
+__createElement__ (_string_ __tag__, _object_ __props__, _string_ __ns__);
+
+_create dom/svg elements_
+
+##### example
+```
+ const container = createElement('div', {
+     id: 'a',
+     style: {
+         color: 'red'
+     },
+     click: () => {
+         this.style.color = 'blue';
+     }
+ })
+```
+
+
+
+
+
+
+
+---
+
+
+
+
+
+
+__updateElement__ (_HTMLElement_ __elem__, _object_ __props__);
+
+_update dom elements_
+
+##### example
+```
+ const element = document.getElementById('asdf');
+ const container = updateElement(element, {
+     style: {
+         color: 'blue'
+     }
+ })
+```
+
+
+
+
+
+
+
+---
+
+
+
+
+
+
+__DOM FACTORY METHODS__
+
+_syntastic sugar_
+
+##### example
+```
+import {DIV, SCRIPT, SPAN} from 'jdom';
+
+ const div = DIV({
+     id: 'myDiv',
+     children: [
+         'injecting script',
+         SCRIPT({src: 'http://some.url'}),
+         'done'
+     ],
+     parent: document.body
+ });
+
+ SPAN({parent: div, children: ['!!!!']);
+```
+
+
+
+
+
+
+
+---
+
+
+
+
+
+
+__SVG FACTORY METHODS__
+
+_syntastic sugar_
+
+##### example
+```
+import {SVG, RECT, CIRCLE} from 'jdom';
+
+ SVG({
+     id: 'mySVG',
+     width: 200,
+     height: 200,
+     viewBox: '0 0 200 200',
+     children: [
+         RECT({
+             fill : 'red',
+             x:5,
+             y:5,
+             width: 190,
+             height: 190
+         }),
+         CIRCLE({
+             fill: 'yellow',
+             cx: 100,
+             cy:100,
+             r:80
+         })
+     ],
+     parent: document.body
+ });
+```
+
+
+
+
+
+
+
+---
+
+
+
+
+
+
+#### aliases
+
+##### example
+```
+const a = id('a');
+const b = $('#b');
+const c = $$('.c');
+```
+
+
+
+
+
+
+---
+
+
+
+
+
+
+#### event management
+
+* __on__ (_HTMLElement_ __elem__, _string_ __event__, _function_ __handler__)
+* __once__ (_HTMLElement_ __elem__, _string_ __event__, _function_ __handler__)
+* __off__ (_HTMLElement_ __elem__, _string_ __event__, _function_ __handler__)
+* __dispatch__ (_HTMLElement_ __elem__, _string_ __event__);
+
+##### example
+```
+ const someDiv = document.getElementById('someDiv');
+ on(someDiv, 'mouseover', () => {
+     someDiv.style.backgroundColor = 'red';
+ });
+ once(someDiv, 'click', () => {
+     someDiv.parentNode.removeChild(someDiv);
+ });
+ off(someDiv, 'click', someFunction);
+ dispatch(someDiv, 'click');
+```
+
+
+
+
+
+
+---
+
+
+
+
+
+
+__currentScript__ ()
+
+_get the currently executing script_
+
+##### example
+
+```
+const thisScript = currentScript();
+console.log(thisScript.src);
+```
+
+
+
+
+
+
+---
+
+
+
+
+
+
+__style__ (_HTMLElement_ __elem__, _object_ __props__)
+
+_update element style_
+
+##### example
+```
+ const someDiv = document.getElementById('someDiv');
+ style(someDiv, {
+     color: 'green',
+     backgroundColor: 'red'
+ })
+```
+
+
+
+
+
+
+
+---
+
+
+
+
+
+
+## utilities
+
+* __isObject__ (_object_ __obj__)
+* __isArray__ (_object_ __obj__)
+* __isElement__ (_object_ __obj__)
+* _private_ __toCamelCase__ (_string_ __str__)
+
+
+
+
+
+
+
+---
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,11 +1,62 @@
+// # JDOM.js
+//
+// _lightweight dom builder_
+//
+// ---
+//
+// ##### installation
+//
+// ```
+// npm install jdom
+//
+// ```
+//
+// #### usage
+//
+// _intended usage with webpack or browserify_
+//
+// ```
+// const {
+//    createElement, updateElement,
+//    DIV, SPAN, SCRIPT, STYLE, // ...etc
+//    id, $, $$,
+//    on, once, off, dispatch,
+//    currentScript,
+//    style,
+//    isObject, isArray, isElement
+// } from 'jdom';
+// ```
+//
+// ---
+// __createElement__ (_string_ __tag__, _object_ __props__, _string_ __ns__);
+//
+// _create dom/svg elements_
+//
+// ##### example
+// ```
+//  const container = createElement('div', {
+//      id: 'a',
+//      style: {
+//          color: 'red'
+//      },
+//      click: () => {
+//          this.style.color = 'blue';
+//      }
+//  })
+// ```
+//
 const createElement = module.exports = function createElement(tag, props, ns) {
-    const elem = typeof ns === 'string'
-        ? document.createElementNS(ns, tag)
-        : document.createElement(tag);
+    const elem = isElement(tag)
+        ? tag
+        : typeof ns === 'string'
+            ? document.createElementNS(ns, tag)
+            : document.createElement(tag);
     if (isObject(props)) {
         Object.keys(props).forEach(function(key) {
             if (key === 'style') {
-                style(elem, props.style);
+                if (isObject(props.style)) {
+                    style(elem, props.style);
+                }
             } else if (key === 'dataset') {
                 if (isObject(props.dataset)){
                     Object.keys(props.dataset).forEach((key) => {
@@ -37,129 +88,240 @@ const createElement = module.exports = function createElement(tag, props, ns) {
     }
     return elem;
 }
-
+// ---
+// __updateElement__ (_HTMLElement_ __elem__, _object_ __props__);
+//
+// _update dom elements_
+//
+// ##### example
+// ```
+//  const element = document.getElementById('asdf');
+//  const container = updateElement(element, {
+//      style: {
+//          color: 'blue'
+//      }
+//  })
+// ```
+//
+module.exports.updateElement = createElement;
+// ---
+// __DOM FACTORY METHODS__
+//
+// _syntastic sugar_
+//
+// ##### example
+// ```
+// import {DIV, SCRIPT, SPAN} from 'jdom';
+//
+//  const div = DIV({
+//      id: 'myDiv',
+//      children: [
+//          'injecting script',
+//          SCRIPT({src: 'http://some.url'}),
+//          'done'
+//      ],
+//      parent: document.body
+//  });
+//
+//  SPAN({parent: div, children: ['!!!!']);
+// ```
+//
 module.exports.META = function META(props) {
     return createElement('meta', props);
 }
-
 module.exports.SCRIPT = function SCRIPT(props) {
     return createElement('script', props);
 }
-
 module.exports.BR = function BR(props) {
     return createElement('br', props);
 }
-
 module.exports.STYLE = function STYLE(props) {
     return createElement('style', props);
 }
-
 module.exports.DIV = function DIV(props) {
     return createElement('div', props);
 }
-
 module.exports.SPAN = function SPAN(props) {
     return createElement('span', props);
 }
-
 module.exports.A = function A(props) {
     return createElement('a', props);
 }
-
 module.exports.I = function I(props) {
     return createElement('i', props);
 }
-
 module.exports.IMG = function IMG(props) {
     return createElement('img', props);
 }
-
 module.exports.VIDEO = function VIDEO(props) {
     return createElement('video', props);
 }
-
 module.exports.IFRAME = function IFRAME(props) {
     return createElement('iframe', props);
 }
-
-// SVG
+// ---
+// __SVG FACTORY METHODS__
+//
+// _syntastic sugar_
+//
+// ##### example
+// ```
+// import {SVG, RECT, CIRCLE} from 'jdom';
+//
+//  SVG({
+//      id: 'mySVG',
+//      width: 200,
+//      height: 200,
+//      viewBox: '0 0 200 200',
+//      children: [
+//          RECT({
+//              fill : 'red',
+//              x:5,
+//              y:5,
+//              width: 190,
+//              height: 190
+//          }),
+//          CIRCLE({
+//              fill: 'yellow',
+//              cx: 100,
+//              cy:100,
+//              r:80
+//          })
+//      ],
+//      parent: document.body
+//  });
+// ```
+//
 const defaultNameSpace = 'http://www.w3.org/2000/svg';
 
 module.exports.SVG = function SVG(props) {
     return createElement('svg', props, defaultNameSpace);
 }
-
 module.exports.DEFS = function DEFS(props) {
     return createElement('defs', props, defaultNameSpace);
 }
-
 module.exports.G = function G(props) {
     return createElement('g', props, defaultNameSpace);
 }
-
 module.exports.CIRCLE = function CIRCLE(props) {
     return createElement('circle', props, defaultNameSpace);
 }
-
 module.exports.RECT = function RECT(props) {
     return createElement('rect', props, defaultNameSpace);
 }
-
 module.exports.PATH = function PATH(props) {
     return createElement('path', props, defaultNameSpace);
 }
-
 module.exports.TEXT = function TEXT(props) {
     return createElement('text', props, defaultNameSpace);
 }
-
 module.exports.ANIMATE = function ANIMATE(props) {
     return createElement('animate', props, defaultNameSpace);
 }
-
 module.exports.ANIMATETRANSFORM = function ANIMATETRANSFORM(props) {
     return createElement('animateTransform', props, defaultNameSpace);
 }
-
-// get current script
-module.exports.currentScript = function currentScript() {
-	if (document.currentScript) {
-		return document.currentScript;
-	}
-	const scripts = document.getElementsByTagName('script');
-	return scripts[scripts.length - 1];
+// ---
+// #### aliases
+//
+// ##### example
+// ```
+// const a = id('a');
+// const b = $('#b');
+// const c = $$('.c');
+// ```
+module.exports.id = id => document.getElementById(id);
+module.exports.$ = selector => document.querySelector(selector);
+module.exports.$$ = selector => document.querySelectorAll(selector);
+// ---
+// #### event management
+//
+// * __on__ (_HTMLElement_ __elem__, _string_ __event__, _function_ __handler__)
+// * __once__ (_HTMLElement_ __elem__, _string_ __event__, _function_ __handler__)
+// * __off__ (_HTMLElement_ __elem__, _string_ __event__, _function_ __handler__)
+// * __dispatch__ (_HTMLElement_ __elem__, _string_ __event__);
+//
+// ##### example
+// ```
+//  const someDiv = document.getElementById('someDiv');
+//  on(someDiv, 'mouseover', () => {
+//      someDiv.style.backgroundColor = 'red';
+//  });
+//  once(someDiv, 'click', () => {
+//      someDiv.parentNode.removeChild(someDiv);
+//  });
+//  off(someDiv, 'click', someFunction);
+//  dispatch(someDiv, 'click');
+// ```
+module.exports.on = (element, event, handler) => {
+    element.addEventListener(event, handler);
 };
-
-// convienence methods
-module.exports.id = document.getElementById;
-module.exports.$ = document.querySelector;
-module.exports.$$ = document.querySelectorAll;
-
-// style
+module.exports.once = (element, event, handler) => {
+    element.addEventListener(event, handler, {once: true});
+};
+module.exports.off = (element, event, handler) => {
+    element.removeEventListener(event, handler);
+};
+module.exports.dispatch = (elem, event) => {
+    const evt = new Event(event);
+    elem.dispatchEvent(evt);
+};
+// ---
+// __currentScript__ ()
+//
+// _get the currently executing script_
+//
+// ##### example
+//
+// ```
+// const thisScript = currentScript();
+// console.log(thisScript.src);
+// ```
+module.exports.currentScript = function currentScript() {
+    if (document.currentScript) {
+        return document.currentScript;
+    }
+    const scripts = document.getElementsByTagName('script');
+    return scripts[scripts.length - 1];
+};
+// ---
+// __style__ (_HTMLElement_ __elem__, _object_ __props__)
+//
+// _update element style_
+//
+// ##### example
+// ```
+//  const someDiv = document.getElementById('someDiv');
+//  style(someDiv, {
+//      color: 'green',
+//      backgroundColor: 'red'
+//  })
+// ```
+//
 const style = module.exports.style = function style(elem, props) {
     if (isElement(elem) && isObject(props)){
         Object.keys(props).forEach((key) => {
-            if (elem.style) {
-                elem.style[key] = props[key];
-            }
+            elem.style[key] = props[key];
         });
     }
 }
-
-function toCamelCase (str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-        return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
-    }).replace(/\s+/g, '');
-}
-
+module.exports.stop = id => {
+    delete animations[id];
+};
+// ---
+// ## utilities
+//
+// * __isObject__ (_object_ __obj__)
+// * __isArray__ (_object_ __obj__)
+// * __isElement__ (_object_ __obj__)
+// * _private_ __toCamelCase__ (_string_ __str__)
+//
 module.exports.isObject = function isObject(obj) {
     return Object.prototype.toString.call(obj).slice(8,-1) === 'Object';
 }
-
 module.exports.isArray = function isArray(obj) {
     return Array.isArray(obj);
 }
-
 module.exports.isElement = function isElement(obj){
     return (
         typeof HTMLElement === "object"
@@ -170,4 +332,10 @@ module.exports.isElement = function isElement(obj){
                 && typeof obj.nodeName==="string"
     );
 }
+function toCamelCase (str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+        return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
+    }).replace(/\s+/g, '');
+}
+// ---
 
